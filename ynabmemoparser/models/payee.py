cae069d63bb0dccbba8bc711d38b7a0c26b9c339
daabel <daniel.basta@here.com>
2024-03-04 16:59:33 +0100

    made category data model simpler, implemented fetch payee method

--- ynabmemoparser/models/payee.py
+++ ynabmemoparser/models/payee.py
@@ -5,3 +5,9 @@ from dataclasses import dataclass
 class Payee:
 	name: str
 	id: str
+	transfer_account_id: str
+
+	@classmethod
+	def from_dict(cls, p_dict: dict):
+		return cls(name=p_dict['name'], id=p_dict['id'], transfer_account_id=p_dict['transfer_account_id'])
+
