94e6d9706a3af0866c70de48c0e16d5587d6a9dc
daabel <daniel.basta@here.com>
2024-03-04 16:58:46 +0100

    made category data model simpler

--- tests/test_categoryrepo.py
+++ tests/test_categoryrepo.py
@@ -3,14 +3,13 @@ from unittest.mock import MagicMock
 import pytest
 
 from ynabmemoparser.exceptions import NoMatchingCategoryError, MultipleMatchingCategoriesError
-from ynabmemoparser.models.category import Category, CategoryGroup
+from ynabmemoparser.models.category import Category
 from ynabmemoparser.repos.categoryrepo import CategoryRepo
 
 
 def test_fetch_by_name_success():
 	cr = CategoryRepo(client=MagicMock())
-	cr.category_groups = [CategoryGroup(id='cg_id', name='cg_name',
-									   categories=frozenset([Category(id='c_id', name='c_name')]))]
+	cr._categories = [Category(id='c_id', name='c_name', group_name='cg_name')]
 	c = cr.fetch_by_name(category_name='c_name')
 	assert c.name == 'c_name'
 
@@ -19,26 +18,21 @@ def test_fetch_by_name_success():
 												  ('c_name', MultipleMatchingCategoriesError)])
 def test_fetch_by_name_fail(test_input, expected):
 	cr = CategoryRepo(client=MagicMock())
-	cr.category_groups = [CategoryGroup(id='cg_id', name='cg_name',
-									   categories=frozenset([Category(id='c_id', name='c_name')])),
-						  CategoryGroup(id='cg_id2', name='cg_name2',
-										categories=frozenset([Category(id='c_id2', name='c_name')]))
-						  ]
+	cr._categories = [Category(id='c_id', name='c_name', group_name='cg_name'),
+					  Category(id='c_id2', name='c_name', group_name='cg_name2')]
 	with pytest.raises(expected):
 		cr.fetch_by_name(category_name=test_input)
 
 
 def test_fetch_by_id_success():
 	cr = CategoryRepo(client=MagicMock())
-	cr.category_groups = [CategoryGroup(id='cg_id', name='cg_name',
-									   categories=frozenset([Category(id='c_id', name='c_name')]))]
+	cr._categories = [Category(id='c_id', name='c_name', group_name='cg_name')]
 	c = cr.fetch_by_id(category_id='c_id')
 	assert c.name == 'c_name'
 
 
 def test_fetch_by_id_fail():
 	cr = CategoryRepo(client=MagicMock())
-	cr.category_groups = [CategoryGroup(id='cg_id', name='cg_name',
-									   categories=frozenset([Category(id='c_id', name='c_name')]))]
+	cr._categories = [Category(id='c_id', name='c_name', group_name='cg_name')]
 	with pytest.raises(NoMatchingCategoryError):
 		cr.fetch_by_id(category_id='xxx')
--- ynabmemoparser/repos/categoryrepo.py
+++ ynabmemoparser/repos/categoryrepo.py
@@ -6,22 +6,22 @@ from ynabmemoparser.models.category import Category
 class CategoryRepo:
 
 	def __init__(self, client: Client):
-		self.category_groups = client.fetch_categories()
+		self._categories = client.fetch_categories()
 
 	def fetch_by_name(self, category_name: str, group_name: str = None) -> Category:
-		cats = [cg for cg in self.category_groups if category_name in [c.name for c in cg.categories]]
+		cats = [c for c in self._categories if category_name == c.name]
 
 		if group_name:
-			cats = [cg for cg in cats if cg.name == group_name]
+			cats = [c for c in cats if c.name == group_name]
 
 		if len(cats) == 1:
-			return next(c for c in cats[0].categories if c.name == category_name)
+			return cats[0]
 		elif len(cats) > 1:
 			raise MultipleMatchingCategoriesError(category_name, cats)
 		raise NoMatchingCategoryError(category_name)
 
 	def fetch_by_id(self, category_id: str) -> Category:
 		try:
-			return next(c for cg in self.category_groups for c in cg.categories if c.id == category_id)
+			return next(c for c in self._categories if c.id == category_id)
 		except StopIteration:
 			raise NoMatchingCategoryError(category_id)
