0cb94f7fb35b858826135b0ab14472c3e6c5558d
dnbasta <daniel.basta@here.com>
2024-03-16 10:04:09 +0100

    created mock transaction with subtransaction option

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,30 @@ from datetime import date
 
 import pytest
 
-from ynabmemoparser.models import OriginalTransaction, Category, Payee
+from ynabmemoparser.models import OriginalTransaction, Category, Payee, OriginalSubTransaction, SubTransaction
+
+
+@pytest.fixture
+def mock_subtransaction(request):
+	return SubTransaction(memo='memo', amount=500,
+						  category=Category(name='cname', id=None),
+						  payee=Payee(name='pname'))
 
 
 @pytest.fixture
 def mock_original_transaction(request):
+	subs = []
 	try:
-		subs = request.param
+		if request.param:
+			ost = OriginalSubTransaction(memo='memo1', amount=500,
+								   category=Category(name='cname', id=None),
+								   payee=Payee(name='pname'))
+			ost2 = OriginalSubTransaction(memo='memo2', amount=500,
+								   category=Category(name='cname', id=None),
+								   payee=Payee(name='pname'))
+			subs = [ost, ost2]
 	except AttributeError as e:
-		subs = []
+		pass
 	return OriginalTransaction(id='id',
 							   memo='memo',
 							   category=Category(id='cid', name='cname'),
