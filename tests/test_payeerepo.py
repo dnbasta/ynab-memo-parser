f81210059d86efcb94e2d9275a0f89b5a2184020
dnbasta <daniel.basta@here.com>
2024-03-17 06:29:18 +0100

    added method to fetch by transfer account id

--- tests/test_payeerepo.py
+++ tests/test_payeerepo.py
@@ -1,27 +1,39 @@
-from unittest.mock import MagicMock
-
 import pytest
 
 from ynabmemoparser.exceptions import NoMatchingPayeeError
 from ynabmemoparser.models.payee import Payee
 from ynabmemoparser.repos.payeerepo import PayeeRepo
 
+
 @pytest.fixture
 def mock_payee_repo():
-	return PayeeRepo(payees=[Payee(id='p_id', name='p_name', transfer_account_id=None),
-							 Payee(id='p_id', name='p_name', transfer_account_id='transfer_id')])
+	return PayeeRepo(payees=[Payee(id='pid1', name='pname1', transfer_account_id=None),
+							 Payee(id='pid2', name='pname2', transfer_account_id='transfer_id')])
 
 
 def test_fetch_payee_by_name_success(mock_payee_repo):
 	# Act
-	p = mock_payee_repo.fetch_payee_by_name(payee_name='p_name')
+	p = mock_payee_repo.fetch_by_name(payee_name='pname2')
 
 	# Assert
 	assert isinstance(p, Payee)
-	assert p.name == 'p_name'
+	assert p.name == 'pname2'
 
 
 def test_fetch_payee_by_name_fail(mock_payee_repo):
 	# Act
 	with pytest.raises(NoMatchingPayeeError):
-		mock_payee_repo.fetch_payee_by_name(payee_name='xxx')
+		mock_payee_repo.fetch_by_name(payee_name='xxx')
+
+
+def test_fetch_by_transfer_account_id_success(mock_payee_repo):
+	# Act
+	r = mock_payee_repo.fetch_by_transfer_account_id('transfer_id')
+	assert isinstance(r, Payee)
+	assert r.id == 'pid2'
+
+
+def test_fetch_by_transfer_account_id_fail(mock_payee_repo):
+	# Act
+	with pytest.raises(NoMatchingPayeeError):
+		mock_payee_repo.fetch_by_transfer_account_id('xxx')
--- ynabmemoparser/repos/payeerepo.py
+++ ynabmemoparser/repos/payeerepo.py
@@ -10,7 +10,7 @@ class PayeeRepo:
 	def __init__(self, payees: List[Payee]):
 		self._payees = payees
 
-	def fetch_payee_by_name(self, payee_name: str) -> Payee:
+	def fetch_by_name(self, payee_name: str) -> Payee:
 		"""Fetches a payee by its name
 
 		:param payee_name: Name of the payee to fetch
@@ -22,7 +22,7 @@ class PayeeRepo:
 		except StopIteration:
 			raise NoMatchingPayeeError(f"No payee with name '{payee_name}")
 
-	def fetch_payee_by_id(self, payee_id: str) -> Payee:
+	def fetch_by_id(self, payee_id: str) -> Payee:
 		"""Fetches a payee by its ID
 
 		:param payee_id: ID of the payee to fetch
@@ -33,6 +33,18 @@ class PayeeRepo:
 			return next(p for p in self._payees if p.id == payee_id)
 		except StopIteration:
 			raise NoMatchingPayeeError(f"No payee with id '{payee_id}")
+
+	def fetch_by_transfer_account_id(self, transfer_account_id: str) -> Payee:
+		"""Fetches a transfer payee by the target account id
+
+		:param transfer_account_id: The id of the target account for the transfer
+		:return: matched transfer payee
+		:raises NoMatchingPayeeError: if no matching payee is found
+		"""
+		try:
+			return next(p for p in self._payees if p.transfer_account_id == transfer_account_id)
+		except StopIteration:
+			raise NoMatchingPayeeError(f"No payee found for transfer_account_id {transfer_account_id}")
 
 	def fetch_all(self) -> List[Payee]:
 		"""Fetches all payees from YNAB budget
