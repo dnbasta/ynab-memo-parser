0f12b113e81487be49e5e5566f69e04b484b5aa1
dnbasta <daniel.basta@here.com>
2024-03-17 09:00:47 +0100

    made more fields optional

--- ynabmemoparser/models/subtransaction.py
+++ ynabmemoparser/models/subtransaction.py
@@ -10,18 +10,19 @@ class SubTransaction(BaseModel):
 	"""YNAB Subtransaction object for creating split transactions. To be used as element in subtransaction attribute of
 	Transaction class
 
+	:ivar amount: The amount of the subtransaction in milliunits
 	:ivar category: The category of the subtransaction
 	:ivar payee: The payee of the subtransaction
-	:ivar amount: The amount of the subtransaction in milliunits
 	:ivar memo: The memo of the subtransaction
 	"""
-	payee: Payee
-	category: Category
-	memo: Optional[str]
 	amount: int
+	payee: Optional[Payee] = None
+	category: Optional[Category] = None
+	memo: Optional[str] = None
 
 	def as_dict(self) -> dict:
 		return dict(payee_id=self.payee.id,
+					payee_name=self.payee.name,
 					category_id=self.category.id,
 					amount=self.amount,
 					memo=self.memo)
